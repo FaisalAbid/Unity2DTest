fba86b64235498b688a496b0c788b226459f575f
Faisal Abid <faisal.abid@gmail.com>
2015-03-21 22:43:24 -0400

    simple game test

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraFollow : MonoBehaviour {
+
+	public GameObject targetObject;
+	private float distanceToTarget;
+	// Use this for initialization
+	void Start () {
+		distanceToTarget = GetComponent<Transform>().position.x - targetObject.transform.position.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float targetObjectX = targetObject.transform.position.x;
+		
+		Vector3 newCameraPosition = GetComponent<Transform>().position;
+		newCameraPosition.x = targetObjectX + distanceToTarget;
+		GetComponent<Transform>().position = newCameraPosition;
+	}
+}
---
+++ Assets/MouseController.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MouseController : MonoBehaviour {
+
+	public float jetpackForce = 75.0f;
+	public float forwardMovementSpeed = 3.0f;
+
+	public Transform groundCheckTransform;
+	
+	private bool grounded;
+	
+	public LayerMask groundCheckLayerMask;
+	
+	Animator animator;
+
+	public ParticleSystem jetpack;
+
+	public bool dead = false;
+
+	// Use this for initialization
+	void Start () {
+		animator =  GetComponent<Animator>();
+	}
+
+	void UpdateGroundedStatus()
+	{
+		//1
+		grounded = Physics2D.OverlapCircle(groundCheckTransform.position, 0.1f, groundCheckLayerMask);
+		
+		//2
+		if (dead) {
+			animator.SetBool ("dead", true);
+		} else {
+			animator.SetBool ("grounded", grounded);
+		}
+	}
+
+
+	void OnTriggerEnter2D(Collider2D collider)
+	{
+		HitByLaser(collider);
+	}
+	
+	void HitByLaser(Collider2D laserCollider)
+	{
+		dead = true;
+	}
+
+
+	// Update is called once per frame
+	// all animation related code
+	void Update () {
+		bool jetpackActive = Input.GetButton("Fire1") && dead == false;
+		
+		if (jetpackActive) {
+			GetComponent<Rigidbody2D> ().AddForce (new Vector2 (0, jetpackForce));
+			Vector2 newVelocity = GetComponent<Rigidbody2D> ().velocity;
+			newVelocity.x = forwardMovementSpeed;
+			GetComponent<Rigidbody2D> ().velocity = newVelocity;
+			jetpack.enableEmission = true;
+		} else {
+			if(dead){
+				jetpack.emissionRate = 100000;
+				jetpack.enableEmission = true;
+			
+			}else{
+				Vector2 newVelocity = GetComponent<Rigidbody2D> ().velocity;
+				newVelocity.x = forwardMovementSpeed;
+				GetComponent<Rigidbody2D> ().velocity = newVelocity;
+				jetpack.enableEmission = false;
+			}
+		}
+
+		UpdateGroundedStatus ();
+	}
+
+	// all physics related code
+	void FixedUpdate () 
+	{
+
+	}
+}
